f9d7517284f7953b37929d485b3a1e9843d37e84
Sefure <sefuresen98@gmail.com>
2019-06-28 15:40:50 +0300

    rails blog oluştur

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+    def index
+      @articles = Article.all
+    end
+   
+    # snippet for brevity
     def index
         @articles = Article.all
       end
